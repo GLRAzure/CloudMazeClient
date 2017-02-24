77beb473dac7c19eb74ff24deeb4702f9ce91ced
peteroden <pete.roden@live.com>
2017-02-24 17:22:11 -0500

    updated wifi connection info and organized the code

--- CloudMazeClient.ino
+++ CloudMazeClient.ino
@@ -29,9 +29,13 @@ const uint8_t ledGridHeight = 3;
 
 WebSocketsClient webSocket;
 const char* chipID;
-const char* wsHost = "192.168.1.249";
-uint32_t wsPort = 3001;
+const char* wsHost = "techo2017.azurewebsites.net";
+uint32_t wsPort = 80;
 int count = 0;
+
+const bool useWifiManager = false;
+const char* wifiSsid = "Tech2017";
+const char* wifiPassword = "expo2017";
 
 Adafruit_NeoPixel pixels = Adafruit_NeoPixel(9, 15, NEO_GRB + NEO_KHZ800);
 
@@ -44,102 +48,116 @@ struct WorldUpdate {
 };
 
 void initSerial() {
-    // Start serial and initialize stdout
-    Serial.begin(115200);
-    //Serial.setDebugOutput(true);
+  // Start serial and initialize stdout
+  Serial.begin(115200);
+  //Serial.setDebugOutput(true);
 }
 void initWifi() {
-
+  if (useWifiManager == true) {
     //WiFiManager
-    //Local intialization. Once its business is done, there is no need to keep it around
     WiFiManager wifiManager;
     //the line below disables the wifi debug messages
-    wifiManager.setDebugOutput(false);
+    wifiManager.setDebugOutput(true);
     //reset saved settings
     //wifiManager.resetSettings();
 
-    //wifiManager.autoConnect("AutoConnectAP");
-    //or use this for auto generated name ESP + ChipID
     wifiManager.autoConnect();
-
     Serial.println("Connected to wifi");
+  } else {
+    Serial.println();
+    Serial.println();
+    Serial.print("Connecting to ");
+    Serial.println(wifiSsid);
+    
+    WiFi.begin(wifiSsid, wifiPassword);
+    
+    while (WiFi.status() != WL_CONNECTED) {
+      delay(500);
+      Serial.print(".");
+    }
+  
+    Serial.println("");
+    Serial.println("WiFi connected");  
+    Serial.println("IP address: ");
+    Serial.println(WiFi.localIP());    
+  }
 }
 
 void initTime() {
-    time_t epochTime;
-    int retries = 0;
-    
-    configTime(0, 0, "pool.ntp.org", "time.nist.gov");
+  time_t epochTime;
+  int retries = 0;
+  
+  configTime(0, 0, "pool.ntp.org", "time.nist.gov");
 
-    while (retries < 30) {
-        epochTime = time(NULL);
+  while (retries < 30) {
+    epochTime = time(NULL);
 
-        if (epochTime == 0) {
-            Serial.println("Fetching NTP epoch time failed! Waiting 2 seconds to retry.");
-            delay(2000);
-            retries++;
-        } else {
-            Serial.print("Fetched NTP epoch time is: ");
-            Serial.println(epochTime);
-            break;
-        }
+    if (epochTime == 0) {
+      Serial.println("Fetching NTP epoch time failed! Waiting 2 seconds to retry.");
+      delay(2000);
+      retries++;
+    } else {
+      Serial.print("Fetched NTP epoch time is: ");
+      Serial.println(epochTime);
+      break;
     }
+  }
 }
 
 void connectWS(const char* host, uint16_t port = 80, const char* path = "/", boolean SSL = false) {
-    Serial.printf("Connecting to server with ID: %s\n", chipID);
-    if (SSL) {
-        webSocket.beginSSL(host, port, path, "", chipID);
-    } else {
-        webSocket.begin(host, port, path, chipID);
-    }
-    webSocket.onEvent(webSocketEvent);
+  Serial.printf("Connecting to server with ID: %s\n", chipID);
+  if (SSL) {
+    webSocket.beginSSL(host, port, path, "", chipID);
+  } else {
+    webSocket.begin(host, port, path, chipID);
+  }
+  webSocket.onEvent(webSocketEvent);
 }
 
 void webSocketEvent(WStype_t type, uint8_t * payload, size_t length) {
-    switch(type) {
-        case WStype_DISCONNECTED:
-            Serial.printf("[WSc] Disconnected!\n");
-            connectWS(wsHost,wsPort);
-            break;
-        case WStype_CONNECTED:
-            Serial.printf("[WSc] Connected to url: %s\n",  payload);
-            // send message to server when Connected
-				    //webSocket.sendTXT("{\"action\":\"Client join\"}");
-            break;
-        case WStype_TEXT:
-      			Serial.printf("Received update from cloud: %s\n", payload);
-      			processCloudMessage((char*)payload);
-      			break;
-        case WStype_BIN:
-            Serial.printf("[WSc] get binary length: %u\n", length);
-            hexdump(payload, length);
-            break;
-    }
+  switch(type) {
+    case WStype_DISCONNECTED:
+      Serial.printf("[WSc] Disconnected!\n");
+      connectWS(wsHost,wsPort);
+      break;
+    case WStype_CONNECTED:
+      Serial.printf("[WSc] Connected to url: %s\n",  payload);
+      // send message to server when Connected
+	    //webSocket.sendTXT("{\"action\":\"Client join\"}");
+      break;
+    case WStype_TEXT:
+			Serial.printf("Received update from cloud: %s\n", payload);
+			processCloudMessage((char*)payload);
+			break;
+    case WStype_BIN:
+      Serial.printf("[WSc] get binary length: %u\n", length);
+      hexdump(payload, length);
+      break;
+  }
 }
 
 void processCloudMessage(char* cloudMessage) {
-    StaticJsonBuffer<200> jsonBuffer;
-    JsonObject& cloudData = jsonBuffer.parseObject(cloudMessage);
-    if (!cloudData.success()) {
-        Serial.println("Malformed JSON from cloud. Blame Andrej");
-        return;
-    }
-    Serial.print("CloudMessage Type: ");
-    const char* type = (const char*)cloudData["type"];
-    Serial.println(type);
-    if (strcmp(type, "world-update") == 0) {
-        WorldUpdate worldInfo = {
-          (const char*)cloudData["player"],
-          (const int)cloudData["pos_x"],
-          (const int)cloudData["pos_y"],
-          (const int)cloudData["player_count"],
-          (const char*)cloudData["surroundings"]
-        };
-        processWorldUpdateMessage(worldInfo);
-    } else {
+  StaticJsonBuffer<200> jsonBuffer;
+  JsonObject& cloudData = jsonBuffer.parseObject(cloudMessage);
+  if (!cloudData.success()) {
+    Serial.println("Malformed JSON from cloud. Blame Andrej");
+    return;
+  }
+  Serial.print("CloudMessage Type: ");
+  const char* type = (const char*)cloudData["type"];
+  Serial.println(type);
+  if (strcmp(type, "world-update") == 0) {
+      WorldUpdate worldInfo = {
+        (const char*)cloudData["player"],
+        (const int)cloudData["pos_x"],
+        (const int)cloudData["pos_y"],
+        (const int)cloudData["player_count"],
+        (const char*)cloudData["surroundings"]
+      };
+      processWorldUpdateMessage(worldInfo);
+  } else {
 
-    }
+  }
 }
 
 void sendActionMessage(char* action, char* direction) {
@@ -170,12 +188,12 @@ void drawPixels(const char* surroundings) {
   for(int i=0;i<9;i++){
     // pixels.Color takes RGB values, from 0,0,0 up to 255,255,255
     switch(surroundings[i]) {
-       case 'w':
-           pixels.setPixelColor(i, pixels.Color(50,0,0));
-           break;
-       case ' ':
-           pixels.setPixelColor(i, pixels.Color(0,0,0));
-           break;
+      case 'w':
+        pixels.setPixelColor(i, pixels.Color(50,0,0));
+        break;
+      case ' ':
+        pixels.setPixelColor(i, pixels.Color(0,0,0));
+        break;
     }
   }
   setPixelCoordColor(1,1, pixels.Color(50,50,50));
@@ -183,50 +201,44 @@ void drawPixels(const char* surroundings) {
 }
 
 void processButtons() {
-  if (upButton.getSingleDebouncedPress())
-  {
+  if (upButton.getSingleDebouncedPress()) {
     Serial.println("Up Pressed");
     sendActionMessage("move", "north");
   }
-  if (downButton.getSingleDebouncedPress())
-  {
+  if (downButton.getSingleDebouncedPress()) {
     Serial.println("Down Pressed");
     sendActionMessage("move", "south");
   }
-  if (leftButton.getSingleDebouncedPress())
-  {
+  if (leftButton.getSingleDebouncedPress()) {
     Serial.println("Left Pressed");
     sendActionMessage("move", "west");
   }
-  if (rightButton.getSingleDebouncedPress())
-  {
+  if (rightButton.getSingleDebouncedPress()) {
     Serial.println("Right Pressed");
     sendActionMessage("move", "east");
   }
-  if (aButton.getSingleDebouncedPress())
-  {
+  if (aButton.getSingleDebouncedPress()) {
     Serial.println("A Pressed");
   }
-  if (bButton.getSingleDebouncedPress())
-  {
+  if (bButton.getSingleDebouncedPress()) {
     Serial.println("B Pressed");
   }
 }
 
 void setup() {
-    String clientName = "ESP" + String(ESP.getChipId());
-    chipID = clientName.c_str();
-        
-    initSerial();
-    initWifi();
-    initTime();
+  String clientName = "ESP" + String(ESP.getChipId());
+  chipID = clientName.c_str();
+      
+  initSerial();
+  initWifi();
+  initTime();
 
-    pixels.begin();
-    
-    connectWS(wsHost,wsPort);
+  pixels.begin();
+  
+  connectWS(wsHost,wsPort);
 }
 
 void loop() {
-    webSocket.loop();
-    processButtons();
+  webSocket.loop();
+  processButtons();
 }
